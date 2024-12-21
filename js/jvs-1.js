b1158a3edf317305f22e9b7f6977de996d462683
Rooster <anh.vhq79@arena-multimedia.edu.vn>
2024-12-22 00:15:05 +0700

    Add files via upload

---
+++ js/jvs-1.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+    const menu = document.querySelector('.menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+
